24c766a5a371ffa7eaf33aedcfcf452cb0ad5c47
akashvermaaaaaa <146636320+akashvermaaaaaa@users.noreply.github.com>
2025-12-30 17:14:47 +0530

    Updated multiple pages: page.jsx, contact.jsx

--- webnex-app/vite-project/src/app/page.jsx
+++ webnex-app/vite-project/src/app/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+                                                                                                                                                                                                                                                                           'use client';
 import React, { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
